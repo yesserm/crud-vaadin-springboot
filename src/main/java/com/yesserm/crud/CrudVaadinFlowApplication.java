3bad11ac345a9ab39e56d0275b325e77a5c8c088
Yesser Miranda <yesser@xenia-pc.nexxtwifi.com>
2024-08-22 16:18:45 -0600

    Initial commit to create project with spring iniatalizr

---
+++ src/main/java/com/yesserm/crud/CrudVaadinFlowApplication.java
@@ -0,0 +1,13 @@
+package com.yesserm.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudVaadinFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudVaadinFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yesserm/crud/CrudVaadinFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yesserm.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudVaadinFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
